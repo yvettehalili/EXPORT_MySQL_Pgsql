493b766d1641782348de67acf8b0088d0d3d1940
yvettehalili <167964506+yvettehalili@users.noreply.github.com>
2024-10-21 13:55:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ if __name__ == "__main__":
     log = Backup_Logger()
     log.process_start()
 
-    print(Database.getServerType("ti-mysql-us-we-14", "ti-ca-infrastructure"))
+    print(Database.getServerType("mysqldbv8", "ti-dba-devenv-01"))
